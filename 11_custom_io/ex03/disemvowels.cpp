499848fa65cbd0dd6bd716fe8114da09a2a47ad4
baviddak <dbak.db@gmail.com>
2022-01-16 11:32:12 -0500

    new ex

--- 11_custom_io/ex03/disemvowels.cpp
+++ 11_custom_io/ex03/disemvowels.cpp
@@ -2,32 +2,46 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <algorithm>
 #include <fstream> 
 
 using namespace std;
+
+void disemvowel(fstream& fs){
+	string new_text;
+
+	vector<char> vowels = {'A','a', 'E', 'e', 'I', 'i', 'O', 'o','U', 'u'};
+
+	// construct a string with no vowels
+	for(char ch; fs>>ch; ){
+		if(*find(vowels.begin(), vowels.end(), ch) == ch) {
+			// do nothing
+    	}else{
+			// add to new string
+			new_text+=ch;
+		} 
+	}
+
+	// overwrite what is in the file
+	cout << new_text;
+
+}
 
 int main() { 
 	// Write a program that removes all vowels
     // from a file
 
-	// Usage:
 	// open an istream
 	cout << "Please ever an input filename\n";
 	string iname;
 	cin >> iname;
-	ifstream ifs{iname};
-	if (!ifs){
+	fstream fs{iname};
+	if (!fs){
 		cout << "Invalid filename\n";
 		return -1;
 	}
 
-
-	// get a word from the user
-	cout << "Please ever word to check\n";
-	string in_word;
-	cin >> in_word;
-
-	line_number(ifs, in_word);
+	disemvowel(fs);
 
 	
 	return 0;
