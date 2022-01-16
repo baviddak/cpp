e0201e4c5c4446cccfd698342e7b6f23afac3a63
baviddak <dbak.db@gmail.com>
2022-01-16 11:48:37 -0500

    a

--- 12_display_model/ex02/build.sh
+++ 12_display_model/ex02/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -Wall -std=c++11 -o howdy.exe howdy.cpp ../../GUI/*.o -I../../GUI/ -lfltk -lfltk_images 
+g++ -Wall -std=c++11 -o howdy.exe howdy.cpp ../../GUI/*.o -I../../GUI/ -lfltk -lfltk_images
