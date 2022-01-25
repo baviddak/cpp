f8a6c9511bedca07624f55af3cb791ec45fe8a70
baviddak <dbak.db@gmail.com>
2022-01-24 23:34:35 -0500

    yeah

--- 09_classes/ex13/main.cpp
+++ 09_classes/ex13/main.cpp
@@ -1,20 +1,20 @@
 #include "Rational.h"
 
 int main() {
-	// do something
-	// implement rationals
+	// Implement rationals ...
 	// Provide assignment, addition, subtraction, 
 	// multiplication, division and equality operators
 
 	Rational aa{34, 3};
 	Rational bb{22, 11};
-	// Rational hh{}
 	Rational cc = aa;
+	Rational ff{1, 3};
 
 	bool test = cc==bb;
 
 	Rational dd = aa + bb;
 	Rational ee = aa - bb;
+	ff = cc * dd;
 
 	cout << "Is c an a same? " << test << "\n";
 	cout << "a plus b is " << dd.denom() << "\n";
--- 10_io_streams/ex05/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ -Wall -std=c++14 -o print_year.exe print_year.cpp
