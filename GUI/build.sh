db559fcfb8233290ad39203aad5429e6bbcb9b70
baviddak <dbak.db@gmail.com>
2022-02-06 23:13:26 -0500

    update

--- GUI/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-g++ -std=c++14 -Wall -c -o Window.o Window.cpp -I.
-g++ -std=c++11 -Wall -c -o Simple_window.o Simple_window.cpp -I.
-g++ -std=c++11 -Wall -c -o GUI.o GUI.cpp -I.
-g++ -std=c++11 -Wall -c -o Graph.o Graph.cpp -I.
