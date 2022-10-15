13ac413476d5bcea82614cabc164d3327e6cf974
baviddak <dbak.db@gmail.com>
2022-10-15 13:07:27 -0400

    fix comment

--- 14_class_design/ex01/main.cpp
+++ 14_class_design/ex01/main.cpp
@@ -2,8 +2,8 @@
 #include "Faces.h"
 
 int main(){
-    //do something
-    using namespace Graph_lib;	// our graphics facilities are in Graph_lib
+    // our graphics facilities are in Graph_lib
+    using namespace Graph_lib;
 
 	Point tl{100,100};		// to become top left corner of window
 	Simple_window win{tl, 600, 600, "Faces Test"};
