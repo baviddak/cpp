5307f854ffa7fac40d5d7fee62c7ede780078232
baviddak <dbak.db@gmail.com>
2022-11-14 19:42:19 -0500

    spaces to tabs

--- 09_classes/ex13/Rational.h
+++ 09_classes/ex13/Rational.h
@@ -6,39 +6,36 @@
 using namespace std;
 
 class Rational {
-    public:
-        Rational(int nn, int dd);
-        ~Rational();
+	public:
+		Rational(int nn, int dd);
+		~Rational();
 
-        // Copy constructor
-	    Rational(const Rational& copy)
+		// Copy constructor
+		Rational(const Rational& copy)
 		: n {copy.numer()}, d {copy.denom()}
-        {
-            // no need to check for a denominator of 0 here since copy must already be a valid Fraction
-            cout << "Copy constructor called\n"; // just to prove it works
-        }
-        
-        // Overloaded assignment
-        Rational& operator= (const Rational& rr);
+		{
+			// no need to check for a denominator of 0 here since copy must already be a valid Fraction
+			cout << "Copy constructor called\n"; // just to prove it works
+		}
+		
+		// Overloaded assignment
+		Rational& operator= (const Rational& rr);
 
-        // arithemic operators
-        Rational& operator+(const Rational& rr);
-        Rational& operator-(const Rational& rr);
-        Rational& operator*(const Rational& rr);
+		// arithemic operators
+		Rational& operator+(const Rational& rr);
+		Rational& operator-(const Rational& rr);
+		Rational& operator*(const Rational& rr);
 
-        int numer() const{return n;};
-        int denom() const{return d;};
+		int numer() const{return n;};
+		int denom() const{return d;};
 
-        double get_dec();
-    private:
-        int n;
-        int d;
+		double get_dec();
+	private:
+		int n;
+		int d;
 };
 
 bool operator==(const Rational& a, const Rational& b);
 bool operator!=(const Rational& a, const Rational& b);
-
-
-
 
 #endif // RATIONAL_H_
