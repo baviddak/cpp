ec479ffdf7a2b4e1c81438a3586203a4cc5f3090
baviddak <dbak.db@gmail.com>
2022-11-20 20:25:31 -0500

    add comments

--- 09_classes/ex05/main.cpp
+++ 09_classes/ex05/main.cpp
@@ -1,7 +1,10 @@
+// This exercise and the next few require you to design and implement a Book 
+// class, such as you can imagine as part of software for a library. Class Book
+// should have members for the ISBN, title, author, and copyright date.
+
 #include "Book.h"
 
 int main() {
-	// do something
 	string hey_there = "bingo bango";
 	Book myBook{"1234", "Thomas", hey_there, false};
 
