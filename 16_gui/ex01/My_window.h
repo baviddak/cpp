aef74991fe2d31d6c2baf3c94be4b405e8a66b7a
baviddak <dbak.db@gmail.com>
2022-11-27 20:19:08 -0500

    correct spaces to tabs

--- 16_gui/ex01/My_window.h
+++ 16_gui/ex01/My_window.h
@@ -8,18 +8,18 @@
 using namespace Graph_lib;
 
 struct My_window : Graph_lib::Window {
-    My_window(Point xy, int w, int h, const string& title);
-    void wait_for_button();
+	My_window(Point xy, int w, int h, const string& title);
+	void wait_for_button();
 
-    private:
-        Button quit_button;
-        Button next_button;
-        bool quit_button_pushed;
-        bool next_button_pushed;
-        static void cb_quit(Address, Address);
-        static void cb_next(Address, Address);
-        void quit();
-        void next();
+	private:
+		Button quit_button;
+		Button next_button;
+		bool quit_button_pushed;
+		bool next_button_pushed;
+		static void cb_quit(Address, Address);
+		static void cb_next(Address, Address);
+		void quit();
+		void next();
 };
 
 #endif // MY_WINDOW_H_
