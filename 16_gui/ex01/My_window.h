5e58822cd629f4f5c6b342165d96be1eab41eb33
baviddak <dbak.db@gmail.com>
2022-11-27 19:14:38 -0500

    Revert "remove 16"
    This reverts commit 10c58016f42a09fe6d0d7abc358797ba5cc1086c.

---
+++ 16_gui/ex01/My_window.h
@@ -0,0 +1,12 @@
+#ifndef MY_WINDOW_H_
+#define MY_WINDOW_H_
+
+#include "Graph.h"
+
+using namespace Graph_lib;
+
+struct My_window : Simple_window {
+    // placeholder
+};
+
+#endif // MY_WINDOW_H_
